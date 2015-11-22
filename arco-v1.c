ae78908e77beb7b3f3968b4ce9e0c0403d4d4526
Sebastian <sebastianls0228@gmail.com>
2015-11-22 11:27:38 -0600

    Take into consideration the four possible archs

--- arco-v1.c
+++ arco-v1.c
@@ -7,6 +7,7 @@ int main () {
     int i, k, centro, coincidencias[4];
     float x, y, ha, hb, ka, kb, x1, x2, x3, y1, y2, y3, r, q, alfa; 
 
+    L: // Introducción de los puntos
     printf("Introduce las coordenadas del primer punto\n");
     scanf("%f%f", &x1, &y1);
     printf("Introduce las coordenadas del segundo punto\n");
@@ -14,7 +15,11 @@ int main () {
     printf("Introduce la magnitud del radio\n");
     scanf("%f", &r);
 
-    q = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)); // Distancia entre los puntos
+    // Distancia entre los puntos y validación de datos
+    if ( r < (q = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)))/2 ) {
+        printf("Los datos no son geométricamente válidos\n"); goto L;
+    }
+
     x3 = (x1+x2)/2; y3 = (y1+y2)/2; // Punto Medio
 
     ha = x3 + sqrt(r*r-(q/2)*(q/2))*(y1-y2)/q; // Centro 1 (X)
@@ -25,24 +30,18 @@ int main () {
 
     alfa = 2*asin((q/2)/r)*(180/M_PI); // Apertura del Arco
         
-    printf("Elige el centro con el que deseas trabajar (1 o 2)\n");
+    N: // Determinación del Centro a usar 
+    printf("Elige el centro del arco que busca (1 o 2):\n");
     printf("Centro 1: (%f, %f)\nCentro 2: (%f, %f)\n", ha, ka, hb, kb);
     scanf("%i", &centro);
-
-    // Determinación del Centro a usar
     if (centro == 1) {
-        x = ha;
-        y = ka;
+        x = ha; y = ka;
         printf("El centro con el que vamos a trabajar es (%f, %f)\n", floor(x), floor(y));
-        printf("El ángulo con el que vamos a trabajar es: %f\n", floor(alfa));
     } else if (centro == 2) {
-        x = hb;
-        y = kb;
+        x = hb; y = kb;
         printf("El centro con el que vamos a trabajar es (%f, %f)\n", floor(x), floor(y));
-        printf("El ángulo con el que vamos a trabajar es: %f\n", floor(alfa));
     } else {
-        printf("%i no es un valor válido\n", centro);
-        scanf("%i", &centro);
+        printf("%i no es un valor válido\n", centro); goto N;
     }
 
     // Coincidencias de puntos dados con un punto en la circunferencia
@@ -53,25 +52,43 @@ int main () {
         float b = r * sin(i * FACTOR_RADIAN) + y;
         if (((fabs(a - x1) < fabs(r*0.0157)) && (fabs(b - y1) < fabs(r*0.0157)))
         || ((fabs(a - x2) < fabs(r*0.0157)) && (fabs(b - y2) < fabs(r*0.0157)))) {
-            coincidencias[k] = i;
-            k++;
+            coincidencias[k] = i; k++;
         }
     }
 
-    // Imprime, adecuadamente, los puntos entre las coincidencias
-    // En caso de que los círculos sean tangentes el arco predeterminado
-    // abarca de 0 a PI radianes
+    M: // Imprime, adecuadamente, los puntos entre las coincidencias
+    printf("Elija el sentido del arco que busca (1 o 2):\n");
+    printf("Desde %i grados (%f, %f) hasta %i grados (%f, %f)\n", 
+        coincidencias[0], r * cos(coincidencias[0] * FACTOR_RADIAN) + x, r * sin(coincidencias[0] * FACTOR_RADIAN) + y,
+        coincidencias[k-1], r * cos(coincidencias[k-1] * FACTOR_RADIAN) + x, r * sin(coincidencias[k-1] * FACTOR_RADIAN) + y
+        );
+    printf("1: En sentido horario\n2: En sentido antihorario\n");
+    scanf("%i", &centro);
+
+    if (centro != 1 && centro != 2) goto M;
     if (fabs(fabs(coincidencias[0] - coincidencias[k-1]) - alfa) < 5) {
         for (i = 0; i < 360; i++) {
-            if ((i >= coincidencias[0]) && (i <= coincidencias[k-1]))
+            if (centro == 2) {
+                if ((i >= coincidencias[0]) && (i <= coincidencias[k-1]))
+                    printf("%i\t(%f, %f)\n", i, r * cos(i * FACTOR_RADIAN) + x, 
+                        r * sin(i * FACTOR_RADIAN) + y);
+            } else {
+                if ((i <= coincidencias[0]) || (360 - coincidencias[k-1] >= 360 - i))
                 printf("%i\t(%f, %f)\n", i, r * cos(i * FACTOR_RADIAN) + x, 
                     r * sin(i * FACTOR_RADIAN) + y);
+            }
         }
     } else {
         for (i = 0; i < 360; i++) {
-            if ((i <= coincidencias[0]) || (360 - coincidencias[k-1] >= 360 - i))
+            if (centro == 2) {
+                if ((i <= coincidencias[0]) || (360 - coincidencias[k-1] >= 360 - i))
                 printf("%i\t(%f, %f)\n", i, r * cos(i * FACTOR_RADIAN) + x, 
                     r * sin(i * FACTOR_RADIAN) + y);
+            } else {
+                if ((i >= coincidencias[0]) && (i <= coincidencias[k-1]))
+                    printf("%i\t(%f, %f)\n", i, r * cos(i * FACTOR_RADIAN) + x, 
+                        r * sin(i * FACTOR_RADIAN) + y);
+            }
         }
     }
 
